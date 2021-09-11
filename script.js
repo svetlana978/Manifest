04c5c9bd8fe8966b2aaf60878d0c23a6b9b8c3c7
svetlana978 <cbeta279@gmail.com>
2021-09-11 17:32:17 +0300

    add slider

---
+++ script.js
@@ -0,0 +1,24 @@
+import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+console.log('ghjklxlkjh');
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
